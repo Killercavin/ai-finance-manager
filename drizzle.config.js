1400b98464b1115555bc0e8792df435caecab073
Cavin <114779273+Killercavin@users.noreply.github.com>
2024-08-26 02:51:03 +0300

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,5 +1,5 @@
-const dbUrl = "postgresql://aifinancetracking_owner:R3mFp4lteKXZ@ep-rough-mountain-a2r7lda9.eu-central-1.aws.neon.tech/aifinancetracking?sslmode=require";
-const dbConnectionString = 'postgresql://aifinancetracking_owner:R3mFp4lteKXZ@ep-rough-mountain-a2r7lda9.eu-central-1.aws.neon.tech/aifinancetracking?sslmode=require';
+const dbUrl = postgresqlDBUrl;
+const dbConnectionString = postgresqlUrl;
 
 export default {
   dialect: "postgresql",
